05603409f3e741d9148a4931d76335fae1b8b6a9
Jess <dev.jessicali@gmail.com>
2024-01-13 15:31:39 -0500

    updated homepage about section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,63 +1,56 @@
 <template>
-    <div>
-      <h1>Welcome to PictoPath Adventures!</h1>      
-      <div>
-        <StoryItem :cardData="cards[currentCard]" @next="goToNextCard" @selectedOption="updateAdventureState"/>
-      </div>
-    </div>
-  </template>
-  
-  <script>
-    import StoryItem from "../components/StoryItem.vue";
-    
-    export default {
-        components: {
-            StoryItem
-        },
-    data() {
-        return {
-            cards: [
-                { 
-                    title: 'Choose your adventure type', 
-                    text: 'Add text', 
-                    options: [
-                        { title: 'Sea Voyage', value: 'Epic sea voyage adventure'},
-                        { title: 'Jungle exploration', value: 'Epic jungle exploration adventure'}],
-                    generateImageFlag: false,
-                    dropdownLabel: 'Select an adventure theme'
-                },
-                { 
-                    title: 'Choose your character', 
-                    text: 'Add text', 
-                    options: [
-                        { title: 'Bunny', value: 'little bunny' },
-                        { title: 'Cat', value: 'lithe kitty' },
-                        { title: 'Dog', value: 'happy puppy' }, ], 
-                    generateImageFlag: false,
-                    dropdownLabel: 'Select a character'
-                },
-                { 
-                    title: 'Pick an item for your trip', 
-                    text: 'Add text', 
-                    options: [
-                        { title: 'French fries', value: 'delicius french fries' },
-                        { title: 'A lantern', value: 'a glowing lantern' },
-                        { title: 'A sun hat', value: 'a yellow straw sun hat' }, ], 
-                    generateImageFlag: false,
-                    dropdownLabel: 'Select a helpful item for you adventure'
-                },
-            ],
-            currentCard: 0
-        };
-    },
-    methods: {
-        goToNextCard() {
-            this.currentCard++;
-        },
-        updateAdventureState(selectedOption) {
-            console.log('Selected Option in Parent:', selectedOption);
-        }
-    }
-};
+  <v-container class="d-flex flex-column align-center justify-center fill-height">
+    <h1>PictoPath Adventures</h1>
+    <h3>A SheHacks+ 8 Project</h3>
+    <br>
+    <img src="../assets/images/banner-2.png" width="500px">
+    <br>
+    <p style="text-align: center;">
+      Welcome to <b>PictoPath Adventures</b>, your ultimate online destination for immersive storytelling!
+      As part of SheHacks+ 8, we want to empower you to shape your own narrative by offering a unique
+      interactive experience where you decide the twists and turns of your adventure. Embark on an epic sea voyage,
+      braving the untamed waves and discovering hidden treasures, or venture deep into the heart of an unexplored jungle,
+      where every step unveils mysteries and challenges.
+    </p>
+    <p>
+      Start your adventure now by going over to the <b>Story page</b>!
+
+    </p>
+    <br>
+    <h3>Submission</h3>
+    <p class="d-flex">
+      Check out our DevPost here! -&nbsp;
+      <a href="https://devpost.com/software/uwu-yjqtrv" target="_blank"
+        class="d-flex align-center git-link">DevPost&nbsp;<v-icon>mdi-open-in-new</v-icon></a>
+    </p>
+    <p class="d-flex">
+      Check out our GitHub repo! -&nbsp;
+      <a href="https://github.com/Li-Jessica/PictoPath-Adventures" target="_blank"
+        class="d-flex align-center git-link">GitHub&nbsp;<v-icon>mdi-open-in-new</v-icon></a>
+    </p>
+    <br>
+    <h3>Developers</h3>
+    <p class="d-flex">
+      Jessica Li - jli3273@uwo.ca -&nbsp;
+      <a href="https://github.com/Li-Jessica" target="_blank"
+        class="d-flex align-center git-link">Github&nbsp;<v-icon>mdi-open-in-new</v-icon></a>
+    </p>
+    <p class="d-flex">
+      Olena Orlova-Kurylova - oorlovak@uwo.ca -&nbsp;
+      <a href="https://github.com/ollenna" target="_blank"
+        class="d-flex align-center git-link">Github&nbsp;<v-icon>mdi-open-in-new</v-icon></a>
+    </p>
+
+  </v-container>
+</template>
+
+<style scoped>
+.git-link {
+  text-decoration: none;
+  color: inherit;
+}
+.v-container {
+  background-color: #f5f3f0;
+}</style>
+<script setup>
 </script>
-  
