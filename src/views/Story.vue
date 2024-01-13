ce15614f43fb36f5e6801fb00005a6c061c198f6
olenna <tabaki11357@gmail.com>
2024-01-13 15:44:23 -0500

    Fixing merge conflics 3

--- src/views/Story.vue
+++ src/views/Story.vue
@@ -32,7 +32,7 @@
                     text: 'Add text', 
                     options: [
                         { title: 'Bunny', value: 'little bunny' },
-                        { title: 'Cat', value: 'lithe kitty' },
+                        { title: 'Cat', value: 'little kitty' },
                         { title: 'Dog', value: 'happy puppy' }, ], 
                     generateImageFlag: false,
                     dropdownLabel: 'Select a character'
